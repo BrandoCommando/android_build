678b9418c3a2608fcd1b8e83c42dde3981231850
Brandon Bowles <BrandonBowles@gmail.com>
2013-08-02 21:13:25 -0700

    Switch to Linaro toolchain 2013-07
    Change-Id: Ib5b498b93737a7217934d7969c1c2ae5cc581d00

--- envsetup.sh
+++ envsetup.sh
@@ -144,7 +144,8 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-4.6/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin
+	arm) toolchaindir=arm/android-toolchain-eabi-4.7/bin
+#        arm) toolchaindir=arm/arm-linux-androideabi-4.6/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-4.6/bin
             ;;
